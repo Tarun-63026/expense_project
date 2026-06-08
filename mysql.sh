1be86165cb8a64989486318bcd900c4cd3942397
Tarun-63026 <lukalapunarru680@gmail.com>
2026-06-08 16:39:36 +0530

    Project

---
+++ mysql.sh
@@ -0,0 +1,39 @@
+#!bin/bash
+
+USERID=$(id -u)
+TIME_STAMP=$(date +F%-H%-M%-S%)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE=/tmp/$SCRIPT_NAME-$TIME_STAMP.log
+
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+VALIDATE () {
+    if [ $1 -ne 0 ]; then
+       echo -e " $2 $R failure.. $N"
+    else
+       echo -e " $2 $G successfully.. $N"
+    fi
+}
+
+if [ $USERID -ne 0 ]; then
+   echo "Please switch to the super user"
+   exit 1
+else
+   echo "You are the super user, please proceed"
+fi
+
+dnf install mysql-server -y &>>LOG_FILE
+VALIDATE $? "Mysql instalation was done"
+
+systemctl enable mysqld &>>LOG_FILE
+VALIDATE $? "Mysql was enabled"
+
+systemctl start mysqld &>>LOG_FILE
+VALIDATE $? "Mysql was started"
+
+mysql_secure_installation --set-root-pass ExpenseApp@1
+VALIDATE $? "Password enabled"
