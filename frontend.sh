30c011a09cacb793a2ecee88e544dcd120023511
Tarun-63026 <lukalapunarru680@gmail.com>
2026-06-08 23:26:32 +0530

    Project

--- frontend.sh
+++ frontend.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 USERID=$(id -u)
-TIME_STAMP=$(date +F%-H%-M%-S%)
+TIME_STAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE=/tmp/$SCRIPT_NAME-$TIME_STAMP.log
 
@@ -28,33 +28,29 @@ else
    echo "You are the super user, please proceed"
 fi
 
-dnf install nginx -y &>>LOG_FILE
+dnf install nginx -y &>>$LOG_FILE
 VALIDATE $? "Installing of nginx"
 
-systemctl enable nginx
+systemctl enable nginx &>>$LOG_FILE
 VALIDATE $? "Enabling of nginx"
 
-systemctl start nginx
+systemctl start nginx &>>$LOG_FILE
 VALIDATE $? "Starting of nginx"
 
-rm -rf /usr/share/nginx/html/*
-VALIDATE $? "removing of deafult/previous content"
+rm -rf /usr/share/nginx/html/* &>>$LOG_FILE
+VALIDATE $? "Removing of default/previous content"
 
-curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip
+curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip &>>$LOG_FILE
 VALIDATE $? "Downloading the frontend content"
 
-cd /usr/share/nginx/html
-VALIDATE $? "Extract the frontend content"
+cd /usr/share/nginx/html &>>$LOG_FILE
+VALIDATE $? "Change directory to Nginx html folder"
 
-unzip /tmp/frontend.zip
-VALIDATE $? "Unzip the content in temporary folder"
+unzip /tmp/frontend.zip &>>$LOG_FILE
+VALIDATE $? "Unzip the content"
 
-cp /home/ec2-user/expense_project/expense.conf  /etc/nginx/default.d/expense.conf
-VALIDATE $? "Coping the content to orignial location"
+cp /home/ec2-user/expense_project/expense.conf /etc/nginx/default.d/expense.conf &>>$LOG_FILE
+VALIDATE $? "Copying the config content to original location"
 
-systemctl restart nginx
-VALIDATE $? "Resatrting the nginx"
-
-
-
-
+systemctl restart nginx &>>$LOG_FILE
+VALIDATE $? "Restarting the nginx"
