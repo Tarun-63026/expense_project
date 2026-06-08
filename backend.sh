f9d6c4df5068562a53fdef98398313ec5988593d
Tarun-63026 <lukalapunarru680@gmail.com>
2026-06-08 22:44:24 +0530

    Project

--- backend.sh
+++ backend.sh
@@ -3,11 +3,10 @@
 USERID=$(id -u)
 TIME_STAMP=$(date +F%-H%-M%-S%)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOG_FILE=/tmp/$SCRIPT_NAME-$TIME_STAMP.log
+LOG_FILE= /tmp/$SCRIPT_NAME-$TIME_STAMP.log
 
-echo "Please end your Database password: "
+echo "Please enter your Database password: "
 read -s DB_password
-
 
 R="\e[31m"
 G="\e[32m"
