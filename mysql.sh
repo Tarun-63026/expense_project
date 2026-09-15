8e0e12c154101a3604982ba16925ae213465bfa2
Tarun-63026 <lukalapunarru680@gmail.com>
2026-09-15 20:33:40 +0530

    db

--- mysql.sh
+++ mysql.sh
@@ -1,9 +1,37 @@
 #!/bin/bash
 
-PERSON1=TARUN
-PERSON2=KAVYA
+USERID=$(id -u)
+TIME_STAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE=/tmp/$SCRIPT_NAME-$TIME_STAMP.log
 
-echo "$PERSON1 :: Hello, $PERSON2!"
-echo "$PERSON2 :: How are you $PERSON1?"
-echo "$PERSON1 :: I am doing fine, I love you $PERSON2"
-echo "$PERSON2 :: I love you too $PERSON1, but I am think to learn Devops."
+R="/e[31m"
+G="/e[32m"
+Y="/e[33m"
+N="/e[0m"
+
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+       echo "$2....$R Failure $N"
+    else
+       echo "$2....$G Success $N"
+    fi   
+}
+
+if [ $USERID -ne 0 ]; then
+  echo -e "$R Please wicth to the super user $N"
+else
+ echo -e "$R You are the super user $N"
+fi 
+
+dnf install mysql-server &>>LOG_FILE
+VALIDATE $? "Installation of mysql"
+
+systemctl enable mysqld &>>LOG_FILE
+systemctl start mysqld &>>LOG_FILE
+VALIDATE $? "Enabling and starting the mysql"
+
+mysql_secure_installation --set-root-pass ExpenseApp@1 &>>LOG_FILE
+VALIDATE $? "Setting the DB password"
+
