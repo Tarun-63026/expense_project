8b689b6c66149e9eb37db647a9dbcf9329e3d335
Tarun-63026 <lukalapunarru680@gmail.com>
2026-09-15 20:20:51 +0530

    db

---
+++ mysql.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=TARUN
+PERSON2=KAVYA
+
+echo "$PERSON1 :: Hello, $PERSON2!"
+echo "$PERSON2 :: How are you $PERSON1?"
+echo "$PERSON1 :: I am doing fine, I love you $PERSON2"
+echo "$PERSON2 :: I love you too $PERSON1, but I am think to learn Devops."
